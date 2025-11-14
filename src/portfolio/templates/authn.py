8621944cc9c4866ad782118a92af58d96abcc2fd
Cedric Anderson Jr <CTANDERSON2@Anderson.edu>
2025-11-14 13:25:41 -0500

    feat: changed int from base10 to base16

--- src/portfolio/templates/authn.py
+++ src/portfolio/templates/authn.py
@@ -1,16 +1,24 @@
 from typing import NewType
+import uuid
+from pydantic import BaseModel
 
 UserID = NewType("UserID", int)
 Username = NewType("Username", str)
 Email = NewType("Email", str)
 Password = NewType("Password", str)
 
-class User:
+class User(BaseModel):
     """ A user of the portfolio system. """
+    id: UserID
+    username: Username
+    email: Email
+    password: Password
 
-    def __init__(self, user_id: UserID, username: Username, email: Email, password: Password) -> None:
-        """Initialize a User."""
-        self.id = user_id
-        self.username = username
-        self.email = email
-        self.password = password
+    @classmethod
+    def create(cls, username: Username, email: Email, password: Password) -> "User":
+        user_id = UserID(uuid.uuid4().int)
+        return cls(id=user_id, username=username, email=email, password=password)
+
+    @property
+    def uuid_obj(self) -> uuid.UUID:
+        return uuid.UUID(int=int(self.id))
