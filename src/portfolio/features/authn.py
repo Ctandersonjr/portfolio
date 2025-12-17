51dfc1fbbe811092cba204cdefed243bc49b05d8
Cedric Anderson Jr <CTANDERSON2@Anderson.edu>
2025-12-17 10:40:52 -0500

    feat: added usersignup

--- src/portfolio/features/authn.py
+++ src/portfolio/features/authn.py
@@ -1,6 +1,7 @@
 from typing import NewType
-from uuid import UUID
+from uuid import UUID, uuid4
 from pydantic import BaseModel, Field, EmailStr
+
 
 UserID = NewType("UserID", int)
 Username = NewType("Username", str)
@@ -9,7 +10,12 @@ Password = NewType("Password", str)
 
 class BaseUser(BaseModel):
     """ A user of the portfolio system. """
-    id: UUID = Field(default_factory=UUID)
+    id: UUID = Field(default_factory=uuid4)
     username: Username = Field(..., min_length=3, max_length=50)
-    email: Email = Field(...)
+    email: EmailStr = Field(...)
     password: Password = Field(..., min_length=8)
+
+class UserSignup(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=8)
+    username: str = Field(..., min_length=3, max_length=50)
